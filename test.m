997f852f351f15a4aa13a4d2e9cf5665c01c1cd6
Gregoire Aubert <gregoire.aubert@heig-vd.ch>
2012-06-11 17:30:44 +0200

    ok je crois que c'est bon

--- test.m
+++ test.m
@@ -11,17 +11,10 @@ function appart(matriceVal, x_max, y_max)
 	fctAppX = [];
 	fctAppY = [];
 	regles = [];
-<<<<<<< HEAD
 	% on selectionne chaque point
 	for n =1: size(matriceVal)(2)
 		% on donne un point et il adapte la matrice des fonctions d'appartenance
 		[fctAppX,fctAppY, regles] = checkPoint(fctAppX,fctAppY, regles, matriceVal(:, n)', x_max, y_max);
-=======
-	% on sélectionne chaque point
-	for n = 1:colonnes
-		% on donne un point et il adapte la matrice des fonctions d'appartenance
-		[fctAppX,fctAppY] = checkPoint(fctAppX,fctAppY, regles, matriceVal(:, n)', x_max, y_max);
->>>>>>> 77923e180030cec991df03c6ca23a56fdad732a1
 	end
 	disp(fctAppX);
 	disp(fctAppY);
@@ -30,22 +23,13 @@ endfunction
 
 % afficher matrice = disp(..)
 
-<<<<<<< HEAD
 % Creer, adapte les fonctions d'appartenance par rapport au point "coord"
 function [newFctX,newFctY, newRegles] = checkPoint(fctAppX,fctAppY,regles, coord, x_max, y_max)
 	
-=======
-% Créer, adapte les fonctions d'appartenance par rapport au point "coord"
-function [newFctX,newFctY] = checkPoint(fctAppX,fctAppY,regles, coord, x_max, y_max)
-	% constante de base pour la création des FAPP
-	deltaUp = 2;
-	deltaDown = deltaUp + 1;
->>>>>>> 77923e180030cec991df03c6ca23a56fdad732a1
 	
 	x = coord(1);
 	y = coord(2);
 	
-<<<<<<< HEAD
 	% Traite un point selon son mu
 	[newFctX, indiceAppX] = checkAppart(x, fctAppX, x_max);
 	[newFctY, indiceAppY] = checkAppart(y, fctAppY, y_max);
@@ -151,97 +135,22 @@ function fctApp = checkMatriceInsersion(fctApp, indice)
 			if (abs(vecteur(4) - fctApp(i,1)) <= delta)
 				% Appondre \  ->\/\
 				% Deplace le haut et le bas				
-=======
-	vecteurX = [];
-	vecteurY = [];
-	
-	[muX, indiceX] = mu(x, fctAppX);
-	[muY, indiceY] = mu(y, fctAppY);
-	
-	if (muX == 0)
-		vecteurX = [max(0,x-deltaDown), max(0,x-deltaUp), min(x+deltaUp,x_max), min(x+deltaDown,x_max)];
-	endif
-	
-	if(muY == 0)
-		vecteurY = [max(0,y-deltaDown), max(0,y-deltaUp), min(y+deltaUp,x_max), min(y+deltaDown,x_max)];
-	endif
-	
-	newFctX = [fctAppX; vecteurX];
-	newFctY = [fctAppY; vecteurY];
-	
-	for i=1:size(fctAppX)
-		newFctX = checkMatriceInsersion(newFctX,i);
-	endfor
-
-endfunction
-
-function pourcent = muLigne(coord, ligne, fctApp)
-	if(coord >= fctApp(ligne,2) & coord <= fctApp(ligne,3))
-		pourcent = 1;
-	elseif(coord >= fctApp(ligne, 1) & coord <= fctApp(ligne,2))
-		l = fctApp(ligne,2) - fctApp(ligne,1);
-		pourcent = coord-fctApp(ligne,1)/l;
-	elseif(coord >= fctApp(ligne, 3) & coord <= fctApp(ligne, 4))
-		l = fctApp(ligne,4) - fctApp(ligne,3);
-		pourcent = 1-(coord-fctApp(ligne,3)/l);
-	else
-		pourcent = 0;
-	end
-end
-
-function [pourcent, indice] = mu(coord, fctApp)
-	pourcent = 0;
-	temp = 0;
-	indice = 0;
-	for n=1:size(fctApp)(1)
-		pourcent = max(pourcent, muLigne(coord, n, fctApp));
-		if pourcent != temp 
-			indice = n;
-		endif
-		temp = pourcent;
-	end
-endfunction
-
-function fctApp = checkMatriceInsersion(fctApp, indice)
-	delta = 0.2;
-	vecteur = fctApp(indice,:);
-	for i=1:size(fctApp)(1)	
-		if i != indice	
-
-				
-			if (abs(vecteur(4) - fctApp(i,1)) <= delta)
-				% Appondre /   ->\/\
-				% Déplace le haut et le bas				
->>>>>>> 77923e180030cec991df03c6ca23a56fdad732a1
 				diff = fctApp(i,1) - vecteur(4);
 				fctApp(indice,4) = fctApp(i,1);
 				fctApp(indice,3) = fctApp(indice,3) + diff;
 				
-<<<<<<< HEAD
 			elseif (vecteur(4) >= fctApp(i,1) && vecteur(4) <= fctApp(i,3))
 				% adapter les 2 points
 				fctApp(indice,4) = fctApp(i,2);
 				fctApp(indice,3) = fctApp(i,1);
 			endif
 			if(abs(vecteur(1)-fctApp(i,4)) <= delta)
-=======
-			elseif (vecteur(4) >= fctApp(i,1) & vecteur(4) <= fctApp(i,3))
-				% adapter les 2 points
-				fctApp(indice,4) = fctApp(i,2);
-				fctApp(indice,3) = fctApp(i,1);
-			
-			elseif(abs(vecteur(1)-fctApp(i,4)) <= delta)
->>>>>>> 77923e180030cec991df03c6ca23a56fdad732a1
 				% Appondre /\/ <-   \
 				diff = fctApp(i,4)-fctApp(indice,1);
 				fctApp(indice,1) = fctApp(i,4);
 				fctApp(indice,2) = vecteur(2) + diff;
 				
-<<<<<<< HEAD
 			elseif(vecteur(1) >= fctApp(i,2) && vecteur(1) <= fctApp(i,4))
-=======
-			elseif(vecteur(1) >= fctApp(i,2) & vecteur(1) <= fctApp(i,4))
->>>>>>> 77923e180030cec991df03c6ca23a56fdad732a1
 				% adapter les deux points
 				fctApp(indice,1) = fctApp(i,3);
 				fctApp(indice,2) = fctApp(i,4);
@@ -249,7 +158,6 @@ function fctApp = checkMatriceInsersion(fctApp, indice)
 			endif
 		endif
 	endfor
-<<<<<<< HEAD
 endfunction
 
 % Apres un changement dans une fonction d'appartenance existante, effectue une modification en chaine des autres fonctions liee
@@ -268,9 +176,3 @@ endfunction
 
 
 appart(dataset,40,40);
-=======
-	
-end
-
-appart(val,10,10);
->>>>>>> 77923e180030cec991df03c6ca23a56fdad732a1
